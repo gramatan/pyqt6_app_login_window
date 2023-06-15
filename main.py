a929db5b57cbb78529dfcc0db06962a7441d2792
gramatan <78417563+gramatan@users.noreply.github.com>
2023-06-15 14:26:44 +0600

    done

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import sys
-import time
 from PyQt6 import QtCore, QtGui, QtWidgets
-
 
 class LoginWindow(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
         # Setup UI
-        self.setWindowTitle("Login App")
+        self.setWindowTitle("Login")
         self.layout = QtWidgets.QVBoxLayout(self)
 
         # PIN code indicator
@@ -21,27 +19,44 @@ class LoginWindow(QtWidgets.QWidget):
 
         # Error message
         self.errorLabel = QtWidgets.QLabel()
+        self.errorLabel.setStyleSheet("color: red")
         self.errorLabel.hide()
         self.layout.addWidget(self.errorLabel)
+        self.layout.setAlignment(self.errorLabel, QtCore.Qt.AlignmentFlag.AlignCenter)
 
         # Custom keyboard
         self.keypadLayout = QtWidgets.QGridLayout()
-        self.keys = [QtWidgets.QPushButton(str(i)) for i in range(0, 10)]
-        self.backspaceButton = QtWidgets.QPushButton("⬅")
+        keys_values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '', '0', '⬅']
+        self.keys = [QtWidgets.QPushButton(str(i)) for i in keys_values if i != '⬅']
+        self.blankButton = QtWidgets.QPushButton('')
+        self.blankButton.setEnabled(False)
         for i, key in enumerate(self.keys):
             key.clicked.connect(self.add_character)
             self.keypadLayout.addWidget(key, i//3, i%3)
+        self.keypadLayout.addWidget(self.blankButton, 3, 0)  # Disabled blank button
+        self.backspaceButton = QtWidgets.QPushButton("⬅")
         self.backspaceButton.clicked.connect(self.remove_character)
         self.keypadLayout.addWidget(self.backspaceButton, 3, 2)
         self.layout.addLayout(self.keypadLayout)
 
         self.pin_code = ""
+        self.reset_required = False
+        self.installEventFilter(self)
+
+    def eventFilter(self, watched, event):
+        if event.type() == QtCore.QEvent.Type.MouseButtonPress and self.reset_required:
+            self.reset_all()
+            self.reset_required = False
+        return super().eventFilter(watched, event)
 
     def add_character(self):
         if len(self.pin_code) < 4:
             self.pin_code += self.sender().text()
             self.indicators[len(self.pin_code) - 1].setText("●")
             if len(self.pin_code) == 4:
+                for key in self.keys:
+                    key.setEnabled(False)
+                self.backspaceButton.setEnabled(False)
                 self.check_pin_code()
 
     def remove_character(self):
@@ -51,10 +66,6 @@ class LoginWindow(QtWidgets.QWidget):
             self.errorLabel.hide()
 
     def check_pin_code(self):
-        for key in self.keys:
-            key.setEnabled(False)
-        self.backspaceButton.setEnabled(False)
-
         if self.pin_code == "1234":
             for indicator in self.indicators:
                 indicator.setStyleSheet("color: green")
@@ -64,9 +75,17 @@ class LoginWindow(QtWidgets.QWidget):
                 indicator.setStyleSheet("color: red")
             self.errorLabel.setText("Invalid PIN. Please try again.")
             self.errorLabel.show()
-            for key in self.keys:
-                key.setEnabled(True)
-            self.backspaceButton.setEnabled(True)
+            self.reset_required = True  # Set the flag to True on error
+
+    def reset_all(self):
+        self.pin_code = ""
+        for indicator in self.indicators:
+            indicator.setStyleSheet("color: black")
+            indicator.setText("○")
+        for key in self.keys:
+            key.setEnabled(True)
+        self.backspaceButton.setEnabled(True)
+        self.errorLabel.hide()
 
     def show_success(self):
         self.close()
